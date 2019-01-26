67a1036f9c63db478e83ed94b2d48fe193309b7e
kidama62 <kidama62@gmail.com>
2019-01-27 00:20:13 +0100

    DEV - strong password

--- src/controllers/RegistrationController.php
+++ src/controllers/RegistrationController.php
@@ -10,6 +10,15 @@ class RegistrationController extends BaseController {
   }
 
   public function postRegistration() {
+
+    if(!$this->validString($_POST['password'])) {
+      $template = $this->twig->load('registration.html');
+      echo $template->render([
+        'error' => 'The password must be at least 6 characters long and contain small letter, capital letter, digit and special character.'
+      ]);
+      die;
+    }
+
     $sql =
     "INSERT INTO user (email, name, password)
     VALUES (?, ?, ?)";
@@ -20,6 +29,15 @@ class RegistrationController extends BaseController {
     // mail("kidama62@gmail.com", "My subject", "pog");
 
     $this->getRegistration();
+  }
+
+  function validString($string) {
+    $containsSmallLetter = preg_match('/[a-z]/', $string);
+    $containsCapsLetter = preg_match('/[A-Z]/', $string);
+    $containsDigit = preg_match('/\d/', $string);
+    $containsSpecial = preg_match('/[^a-zA-Z\d]/', $string);
+    $length = strlen($string) >= 6;
+    return ($containsSmallLetter && $containsCapsLetter && $containsDigit && $containsSpecial && $length);
   }
 
 }
